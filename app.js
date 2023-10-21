8348c017e939496b05113e264040816345938b13
mak7ytka <65386630+mak7ytka@users.noreply.github.com>
2023-10-21 13:02:09 +0500

    Update app.js

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ btn6.addEventListener("click", function(){
 
 Telegram.WebApp.onEvent("mainButtonClicked", function(){
 	tg.sendData(item);
-	httpGet("http://193.124.117.116:5000/ai-quotes/5/5332895683");
+	fetch('http://193.124.117.116:5000/ai-quotes/5/5332895683');
 });
 
 
